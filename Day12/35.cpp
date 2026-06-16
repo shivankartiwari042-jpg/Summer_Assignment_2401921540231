f54141ee3728a1a397700ff894e55dfb1e292bb2
shiva <shivankartiwari042@gmail.com>
2026-06-16 20:24:28 +0530

    Day 2 of 3rd week

---
+++ Day12/35.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* fast = &dummy;
+        ListNode* slow = &dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast->next;
+        }
+
+        while (fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        ListNode* nodeToDelete = slow->next;
+        slow->next = slow->next->next;
+        delete nodeToDelete;
+
+        return dummy.next;
+    }
+};
---
+++ Day12/36.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = reverse(slow);
+        ListNode* first = head;
+
+        while (second) {
+            if (first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
